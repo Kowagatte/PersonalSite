25f5ace6475a1bd14d0938c249810eff4018ecc7
Nick Ryan <nnryanp@gmail.com>
2021-03-31 23:04:06 -0600

    Added font-awesome to project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 import './index.css';
+
+export const Beverage = () => (
+    <div id={"font-awesome"}>
+        Your <FontAwesomeIcon icon={faCoffee}/> is hot and ready!
+    </div>
+)
 
 class MainPage extends React.Component{
     render() {
         return(
             <div>
                 <Navbar/>
+                <Beverage/>
                 <Footer/>
             </div>
         );
