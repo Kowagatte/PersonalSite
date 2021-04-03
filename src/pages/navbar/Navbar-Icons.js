197b56aee17d615e563281885489979f9257637c
Nick Ryan <nnryanp@gmail.com>
2021-04-03 16:35:46 -0600

    Made navbar components modular and added icons for them

---
+++ src/pages/navbar/Navbar-Icons.js
@@ -0,0 +1,25 @@
+import {
+    faBook,
+    faFolderOpen,
+    faUserEdit
+} from '@fortawesome/free-solid-svg-icons'
+
+import {
+} from '@fortawesome/free-brands-svg-icons'
+
+let icons = {
+    "Devlog": {
+        "icon": faBook,
+        "href": "/d/"
+    },
+    "Projects": {
+        "icon": faFolderOpen,
+        "href": "/p/"
+    },
+    "About Me": {
+        "icon": faUserEdit,
+        "href": "/am/"
+    }
+}
+
+export {icons};
--- src/pages/navbar/Navbar.js
+++ src/pages/navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './navbar.css';
+import { icons } from './Navbar-Icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 class Navbar extends React.Component {
     render() {
@@ -7,9 +9,18 @@ class Navbar extends React.Component {
             <div className="top-navbar">
                 <div className="navbar-header"><a href={'/'}>Damocles</a></div>
                 <ul className="navbar-objects">
-                    <li className="navbar-objects"><a href={'/am/'}>About Me</a></li>
-                    <li className="navbar-objects"><a href={'/p/'}>Projects</a></li>
-                    <li className="navbar-objects"><a href={'/d/'}>Devlog</a></li>
+                    {
+                        Object.keys(icons).map((key, index)=>{
+                            return <li className={'navbar-objects'}>
+                                <a href={icons[key].href}>
+                                    <FontAwesomeIcon className={'navbar-objects'} icon={icons[key].icon}/>
+                                    <div className={'navText'}>
+                                        {key}
+                                    </div>
+                                </a>
+                            </li>
+                        })
+                    }
                 </ul>
             </div>
         );
