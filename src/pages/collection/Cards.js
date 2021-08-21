8736ac94c3867ffa84e4c6280bc928252ca46d0d
Nick Ryan <nnryanp@gmail.com>
2021-08-20 18:58:29 -0600

    Update Cards.js
    Updated collection

--- src/pages/collection/Cards.js
+++ src/pages/collection/Cards.js
@@ -4,7 +4,11 @@ let cards = {
     "sta/89": false,
     "sta/117": false,
     "sta/116": false,
-    "sta/82": false
+    "sta/82": false,
+    "sta/120": false,
+    "sta/112": false,
+    "sta/66": false,
+    "sta/107": true
 }
 
 export {cards};
