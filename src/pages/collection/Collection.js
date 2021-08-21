09a1296f07a2c89da4bce36cb63d07b1a3da6aa3
Nick Ryan <nnryanp@gmail.com>
2021-08-20 18:59:09 -0600

    Update Collection.js
       - Rounded total to 2 decimal places
       - Added foil flag and proper pricing

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -10,27 +10,33 @@ function constructFetchLink(text){
 function sumPrices(cardData){
     let total = 0.0;
     for(const card of cardData){
-        total += parseFloat(card.prices.usd)
+        total += parseFloat(card.price)
     }
-    return total
+    return total.toFixed(2)
 }
 
 class Collection extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            card_data: [],
-            prices: []
+            card_data: []
         }
     }
 
     componentDidMount() {
         for(const [key, value] of Object.entries(cards)){
+            let card = {"name": null, "lang": null, "img": null, "price": 0.0, "foil": false}
             fetch(constructFetchLink(key))
                 .then(response => response.json())
                 .then(data => {
+                    card["lang"] = data.lang
+                    card["name"] = data.name
+                    card["img"] = data.image_uris.png
+                    card["foil"] = value
+                    card["price"] = value ? data.prices.usd_foil : data.prices.usd;
+
                     this.setState(prevState => ({
-                        card_data: [...prevState.card_data, data]
+                        card_data: [...prevState.card_data, card]
                     }))
                 });
         }
@@ -47,9 +53,9 @@ class Collection extends React.Component{
                         const data = card_data[index]
                         return <Card
                             name={data.name}
-                            image_url={data.image_uris.png}
+                            image_url={data.img}
                             lang={data.lang}
-                            price={data.prices.usd}
+                            price={data.price}
                         />
                     })
                 }
