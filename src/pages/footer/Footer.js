66ae39385065c5de52c17af8424690c96277a02a
Nicholas Ryan <nnr635@usask.ca>
2023-03-20 12:09:33 -0600

    Moved footer icons to be inline with text

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -26,17 +26,18 @@ class Footer extends React.Component{
         return(
             <div className={"footer"}>
 
-                <div className={'fontAwesomeIcons'}>
-                    {
-                        Object.keys(icons).map((key, index)=>{
-                            return <Icon href={icons[key].href} icon={icons[key].icon}/>
-                        })
-                    }
-                </div>
                 <ul className={'footer-notes'}>
-                    <li id={'id-tag'} className={'footer-notes'}>Nicholas N Ryan</li>
+                    <li id={'id-tag'} className={'footer-notes'}>Nicholas Ryan</li>
                     <li className={'footer-notes'}>•</li>
-                    <li id={'id-tag'} className={'footer-notes'}>2019-2022</li>
+                    <li className={'footer-icons'}>
+                        {
+                            Object.keys(icons).map((key, index)=>{
+                                return <Icon href={icons[key].href} icon={icons[key].icon}/>
+                            })
+                        }
+                    </li>
+                    <li className={'footer-notes'}>•</li>
+                    <li id={'id-tag'} className={'footer-notes'}>2019-2023</li>
                     <li className={'footer-notes'}>•</li>
                     <li className={'footer-notes'}><a href={'https://damocles.ca'}>damocles.ca</a></li>
                 </ul>
